d0f246388c622a8e74df7c604585765eb4d9fffb
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-23 00:30:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,13 @@ class Song
   attr_accessor :artist, :genre
   attr_reader :name 
   
+  @@all = []
+  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre
+    @@all << self 
   end 
   
 end 
