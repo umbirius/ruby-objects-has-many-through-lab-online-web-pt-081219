78e56d366418cdd2ef6b3c1f848452496f5f040b
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-23 00:42:40 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,17 @@
 class Appointment
   
+  attr_accessor 
+  attr_reader :date 
   
+  @@all = []
+  
+  def initialize(date)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+end 
