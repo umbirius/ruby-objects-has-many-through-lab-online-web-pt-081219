5d870c1b7f67425a86ffb5f1ebd935a563c4443b
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-23 00:37:06 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,8 +2,15 @@ class Genre
   
   attr_reader :name 
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
   end 
   
   def songs 
